bac654206e1dec26535140f6546de8294bf80b7e
nmli1992 <nmli1992@gmail.com>
2019-10-10 09:55:30 +0900

    상품종류 행 하나 빠진거 수정

--- project/WebContent/table/Goods.sql
+++ project/WebContent/table/Goods.sql
@@ -11,3 +11,4 @@ insert into goods values(1, 2, '도시락');
 insert into goods values(1, 3, '볶음밥');
 insert into goods values(2, 4, '닭가슴살');
 insert into goods values(2, 5, '도시락');
+insert into goods values(2, 6, '피자');
