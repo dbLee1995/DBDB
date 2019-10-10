9a4f183b886c94c9cd4242813c8635924175934b
nmli1992 <nmli1992@gmail.com>
2019-10-10 11:08:21 +0900

    goods 테이블 gdlistnum 5번 기존 도시락에서 소세지로 변경

--- project/WebContent/table/Goods.sql
+++ project/WebContent/table/Goods.sql
@@ -10,5 +10,7 @@ insert into goods values(1, 1, '닭가슴살');
 insert into goods values(1, 2, '도시락');
 insert into goods values(1, 3, '볶음밥');
 insert into goods values(2, 4, '닭가슴살');
-insert into goods values(2, 5, '도시락');
+insert into goods values(2, 5, '소세지');
 insert into goods values(2, 6, '피자');
+
+
