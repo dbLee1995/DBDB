933a01723c7f790f49fa31c659a5cfed80fe0066
nmli1992 <nmli1992@gmail.com>
2019-10-10 09:49:14 +0900

    회사종류 행2개 ,상품종류 행6개 추가

--- project/WebContent/table/Goods.sql
+++ project/WebContent/table/Goods.sql
@@ -4,5 +4,10 @@ create table goods
 	GDlistNum number(10) primary key,
 	GDlist varchar2(20)
 );
-
 CREATE SEQUENCE goods_SEQ;
+
+insert into goods values(1, 1, '닭가슴살');
+insert into goods values(1, 2, '도시락');
+insert into goods values(1, 3, '볶음밥');
+insert into goods values(2, 4, '닭가슴살');
+insert into goods values(2, 5, '도시락');
--- project/WebContent/table/company.sql
+++ project/WebContent/table/company.sql
@@ -5,3 +5,6 @@ create table company
 	phone number(20)
 );
 CREATE SEQUENCE company_SEQ;
+
+insert into company values(1, '맛있닭', 07089845571);
+insert into company values(2, '푸드원', 07049911458);
