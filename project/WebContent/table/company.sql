0812e40a5a70850f94a44c577e3e42f2b06f7fef
nmli1992 <nmli1992@gmail.com>
2019-10-11 17:27:35 +0900

    테스트용 phone 오탈자 수정

--- project/WebContent/table/company.sql
+++ project/WebContent/table/company.sql
@@ -6,8 +6,8 @@ create table company
 );
 CREATE SEQUENCE company_SEQ;
 
-insert into company values(1, '맛있닭', 07089845571);
-insert into company values(2, '푸드원', 07049911458);
+insert into company values(1, '맛있닭', '07089845571');
+insert into company values(2, '푸드원', '07049911458');
 
 --
 delete from shoppinglist;
