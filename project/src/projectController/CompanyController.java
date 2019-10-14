c70d72d0a835c4df0cf78e59654750d8b6159c3f
nmli1992 <nmli1992@gmail.com>
2019-10-14 15:24:46 +0900

    companycontroller 추가

--- project/src/projectController/CompanyController.java
+++ project/src/projectController/CompanyController.java
@@ -17,6 +17,7 @@ import projectVo.CompanyVo;
 public class CompanyController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
 		String cmd=req.getParameter("cmd");
 		if(cmd!=null && cmd.equals("cpCheck")) {
 			cpCheck(req,resp);
@@ -27,6 +28,7 @@ public class CompanyController extends HttpServlet{
 		
 	}
 	protected void cpInsert(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain;charset=utf-8");
 		String cpName=req.getParameter("cpName");
 		String cpPhone=req.getParameter("cpPhone");
 		CompanyDao dao=CompanyDao.getInstance();
@@ -40,14 +42,13 @@ public class CompanyController extends HttpServlet{
 		resp.setContentType("text/plain;charset=utf-8");
 		String cpName=req.getParameter("cpName");
 		boolean using=false;
+		JSONObject json=new JSONObject();
 		CompanyDao dao=CompanyDao.getInstance();
 		CompanyVo vo=new CompanyVo(0, cpName, null);
 		vo=dao.select(cpName);
-		JSONObject json=new JSONObject();
 		if(vo!=null) {
-			using = true;
+			using=true;
 		}
-		
 		json.put("using", using);
 		PrintWriter pw=resp.getWriter();
 		pw.print(json.toString());
