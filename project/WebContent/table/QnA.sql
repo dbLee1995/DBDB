afe6cbec18c0ef3b6ccc7fd1768e8521a77253d7
nmli1992 <nmli1992@gmail.com>
2019-10-21 11:28:49 +0900

    QnA 테이블 title 100byte 변경 answer/content 100byte->200byte 변경

--- project/WebContent/table/QnA.sql
+++ project/WebContent/table/QnA.sql
@@ -3,9 +3,9 @@ create table QnA
 	id varchar2(20) references account(id),
 	category varchar2(20),
 	title varchar2(20),
-	content varchar2(100),
+	content varchar2(200),
 	regdate date,
-	answer varchar2(100),
+	answer varchar2(200),
 	answerdate date,
 	answerstate number(5)
 );
